11e6c1fa33f48ea5988f8c49cd79f01d050ec959
SimerpreetSingh9 <callsimer@gmail.com>
2020-08-15 15:55:36 +0200

    Adding more basic test cases and code

--- src/com/game/code/Position.java
+++ src/com/game/code/Position.java
@@ -1,5 +1,9 @@
 package com.game.code;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Position {
 
 	public static final int DIMENSION = 3;
@@ -16,6 +20,13 @@ public class Position {
 		}
 	}
 	
+	
+	public Position(String board, char turn) {
+		this.board = board.toCharArray();
+		this.turn = turn;
+	}
+
+
 	public Position move(int index)
 	{
 		board[index] = turn;
@@ -27,5 +38,74 @@ public class Position {
 	public String toString()
 	{
 		return new String(board);
+	}
+
+	public Position unmove(int index) {
+		board[index] = ' ';
+		turn = turn == 'x' ? 'o' : 'x';
+		return this;
+	}
+	
+	public List<Integer> possibleMoves() {
+		List<Integer> listOfInt = new ArrayList<Integer>();
+		for (int i = 0; i < board.length; i++)
+		{
+			if (board[i] == ' ')
+				listOfInt.add(i);
+		}
+		return listOfInt;
+	}
+
+	public boolean isGameWonBy(char turn) {
+		boolean isWin = false;
+		// test win for xxx in row
+		for(int i=0; i< SIZE ; i+= DIMENSION) {
+			isWin = isWin || MatchALine(turn, i, i+DIMENSION , 1);
+		}
+		// test win for xxx in column
+		for(int i=0; i< DIMENSION ; i++) {
+			isWin = isWin || MatchALine(turn, i, SIZE , DIMENSION);
+		}
+		// test for diagonal
+		isWin = isWin || MatchALine(turn, 0, SIZE, DIMENSION+1);
+		// test for diagonal
+		isWin = isWin || MatchALine(turn, DIMENSION-1, SIZE-1, DIMENSION-1);
+		return isWin;
+	}
+
+
+	private boolean MatchALine(char turn, int startIndex, int endIndex, int step) {
+		for(int i= startIndex; i < endIndex; i+= step)
+		{
+			if(board[i] != turn)
+				return false;
+		}
+		return true;
+	}
+
+	public int blanks() {
+		int total = 0;
+		for(int i=0; i< SIZE; i++)
+		{
+			if(board[i] == ' ')
+				total++;
+		}
+	return total;
+	}
+	public int minmax() {
+		if(isGameWonBy('x')) 
+			return blanks();
+		if(isGameWonBy('o')) 
+			return -blanks();
+		if(blanks() == 0) 
+			return 0;
+		// recursive cases
+		List<Integer> list = new ArrayList<Integer>();
+		for(Integer idx : possibleMoves()) {
+			list.add(move(idx).minmax());
+			unmove(idx);
+		}
+		// handle  0 recursion cases
+		return turn == 'x' ? Collections.max(list) : Collections.min(list);
 	}
 }
--- src/com/game/test/PositionTest.java
+++ src/com/game/test/PositionTest.java
@@ -1,8 +1,12 @@
 package com.game.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +14,10 @@ import com.game.code.Position;
 
 class PositionTest {
 
-	private static Position position;
-	
-	@BeforeAll
-	static void init()
-	{
-		position = new Position();
-	}
-	
 	@Test
+	@DisplayName("Testing new position")
 	void testNewPosition() throws Exception {
+		Position position = new Position();
 		assertEquals('x', position.turn);
 		assertEquals("         ", position.toString());
 	}
@@ -27,9 +25,70 @@ class PositionTest {
 	@Test
 	@DisplayName("Testing move by x at index 1")
 	void testMove() throws Exception {
+		Position position = new Position();
 		position = position.move(1);
 		assertEquals('o', position.turn);
 		assertEquals(" x       ", position.toString());
 	}
 	
+	@Test
+	@DisplayName("Testing unmove by x at index 1")
+	public void testUnmove() throws Exception{
+		Position position = new Position();
+		position = position.move(1).unmove(1);
+		assertEquals('x', position.turn);
+		assertEquals("         ", position.toString());
+	}
+	
+	@Test
+	@DisplayName("Testing possible moves")
+	public void testPossibleMoves() throws Exception {
+		List<Integer> listOfInt = new ArrayList<Integer>();
+		Position position = new Position();
+		for( int i=0; i< position.SIZE;i++) {
+			listOfInt.add(i);
+		}
+		listOfInt.remove(new Integer(1));
+		listOfInt.remove(new Integer(2));
+		assertEquals(listOfInt, position.move(1).move(2).possibleMoves());
+	}	
+	
+	@Test
+	@DisplayName("Testing if game is won as per possible moves")
+	public void testIsGameWonBy() throws Exception {
+
+		assertFalse(new Position().isGameWonBy('x'));
+		//testing for win xxx in a row
+		assertTrue(new Position("xxx      ",'x').isGameWonBy('x'));
+		//testing for win xxx in column
+		assertTrue(new Position(
+				 "x  "
+				+"x  "
+				+"x  ",'x').isGameWonBy('x'));
+		//testing for win ooo in diagnoal
+		assertTrue(new Position(
+				 "o  "
+				+" o "
+				+"  o",'x').isGameWonBy('o'));
+		//testing for win ooo in diagnoal
+		assertTrue(new Position(
+				 "  o"
+				+" o "
+				+"o  ",'x').isGameWonBy('o'));
+	}
+	@Test
+	public void testMinMax() throws Exception
+	{
+		// return how many blank spaces are
+		assertEquals(6, new Position("xxx      ",'x').minmax());
+		// negative value if win for o
+		assertEquals(-6, new Position("ooo      ",'o').minmax());
+		// zero if draw
+		assertEquals(0, new Position("xoxxoxoxo",'x').minmax());
+		
+		// test recursive cases
+		assertEquals(6, new Position("xx       ",'x').minmax());
+		// neg value if win for o
+	   assertEquals(-6, new Position("oo       ",'o').minmax());
+	}
 }
