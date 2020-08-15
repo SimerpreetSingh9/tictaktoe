bf28725f3a57d56d313d105fdab682afab9be291
SimerpreetSingh9 <callsimer@gmail.com>
2020-08-15 14:08:03 +0200

    Test new position

--- src/com/game/code/Position.java
+++ src/com/game/code/Position.java
@@ -2,4 +2,23 @@ package com.game.code;
 
 public class Position {
 
+	public static final int DIMENSION = 3;
+	public static final int SIZE = DIMENSION*DIMENSION;
+	public char turn;
+	public char[] board;
+
+	public Position() {
+		this.turn = 'x';
+		board = new char[SIZE];
+		for(short i=0 ; i< SIZE; i++)
+		{
+			board[i]=' ';
+		}
+	}
+
+	@Override
+	public String toString()
+	{
+		return new String(board);
+	}
 }
--- src/com/game/test/PositionTest.java
+++ src/com/game/test/PositionTest.java
@@ -2,13 +2,24 @@ package com.game.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import com.game.code.Position;
 
 class PositionTest {
 
+	private static Position position;
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testNewPosition() throws Exception {
+		assertEquals('x', position.turn);
+		assertEquals("         ", position.toString());
+	}
+	
+	@BeforeAll
+	static void init()
+	{
+		position = new Position();
 	}
 
 }
